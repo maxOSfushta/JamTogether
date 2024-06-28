9ffa9203c5125898437be3fd84bdd415206762f9
maxOSfushta <fushta.m1997333@gmail.com>
2024-06-28 08:18:50 +0200

    init commit

---
+++ src/main/java/com/jamTogether/appmusic/AppmusicApplication.java
@@ -0,0 +1,13 @@
+package com.jamTogether.appmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmusicApplication.class, args);
+	}
+
+}
